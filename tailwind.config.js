a2149e64bae12ef67c232d0cdd0f1a14b1116ab2
Cole Brown <colemanbrown@Coles-MacBook-Air.local>
2026-03-29 23:04:50 -0400

    Set Manrope as app font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Manrope', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
       colors: {
         primary: { 50:'#f0fdf4', 100:'#dcfce7', 200:'#bbf7d0', 300:'#86efac', 400:'#4ade80', 500:'#22c55e', 600:'#16a34a', 700:'#15803d', 800:'#166534', 900:'#14532d' },
         success: { 50:'#f0fdf4', 500:'#22c55e', 700:'#15803d' },
